0df94b7dddc2001d6be090eb8a8a535b5d255d04
HugoSilva92 <hugo.f.o.silva@gmail.com>
2023-08-14 01:13:04 +0100

    Declared comparison and arithmetic operator in HEADER, still need incremnt and decrement!

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,37 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed{
+	private:
+		int	_fixedPoint;
+		static const int _fractionalBit = 8;
+	public:
+		Fixed(void);
+		Fixed(Fixed const & num);
+		Fixed& operator= (Fixed const & num);
+		Fixed& operator== (Fixed const & num);
+		Fixed& operator>= (Fixed const & num);
+		Fixed& operator> (Fixed const & num);
+		Fixed& operator<= (Fixed const & num);
+		Fixed& operator< (Fixed const & num);
+		Fixed& operator!= (Fixed const & num);
+		Fixed& operator* (Fixed const & num);
+		Fixed& operator/ (Fixed const & num);
+		Fixed& operator+ (Fixed const & num);
+		Fixed& operator- (Fixed const & num);
+		Fixed(const int num);
+		Fixed(const float num);
+		~Fixed();
+
+		float	toFloat(void) const;
+		int		toInt(void) const ;
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+
+std::ostream & operator<<(std::ostream & out, Fixed const & num);
+
+#endif
