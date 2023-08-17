f276f32c8055fe6f0d54e4e0c82047393b54ab68
Hugo Fernando Oliveira Silva <huolivei@c1r3s7.42porto.com>
2023-08-17 11:12:22 +0100

    removed extra files

--- bsp.cpp
+++
@@ -1,7 +0,0 @@
-#include "Point.hpp"
-
-bool bsp( Point const a, Point const b, Point const c, Point const point){
-    // a b c are the vertices of our triagle;
-    // point is the point to check;
-    // returns true if its in there false otherwise;
-}
